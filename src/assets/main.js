d944ae98215c048ae751c71c47f678ac83af9adf
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2017-08-20 23:22:36 +0200

    resolve exercise

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
   // your code will go here
+  $.ajax('https://www.codeschool.com/users/1446365.json', {
+    method: 'GET',
+    dataType: 'jsonp',
+    success: ({courses}) => {
+      $('#badges').append(courses.completed.map(({title, url, badge}) =>
+        $(`<div class="course">
+            <h3>
+              ${title}
+            </h3>
+            <img src="${badge}" />
+            <a href="${url}">Link to course</a>
+          </div>`)))
+    }
+  })
 
 });
